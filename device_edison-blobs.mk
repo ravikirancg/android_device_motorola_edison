ee54145b273bdef666a574b96ad17e82fc1dcfab
alteredlikeness <alteredlikeness@gmail.com>
2013-03-16 18:05:42 -0500

    update device blobs, added temp prebuilts

--- device_edison-blobs.mk
+++ device_edison-blobs.mk
@@ -1,3 +1,5 @@
+DEVICE_BLOB_FOLDER := device/motorola/edison/prebuilt
+
 # Audio - use stock ICS leak files for now
 PRODUCT_COPY_FILES += \
     device/motorola/edison/audio/alsa.omap4.so:/system/lib/hw/alsa.omap4.so \
@@ -8,7 +10,7 @@ PRODUCT_COPY_FILES += \
     device/motorola/edison/audio/librds_util.so:/system/lib/librds_util.so \
     device/motorola/edison/audio/libril_rds.so:/system/lib/libril_rds.so
     
-    # Permissions files
+# Permissions files
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:/system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:/system/etc/permissions/android.hardware.camera.front.xml \
@@ -28,73 +30,73 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
 
-    # Prebuilts
+# Prebuilts
 PRODUCT_COPY_FILES += \
-    device/motorola/edison/prebuilt/bin/battd:system/bin/battd \
-    device/motorola/edison/prebuilt/bin/mount_ext3.sh:system/bin/mount_ext3.sh \
-    device/motorola/edison/prebuilt/bin/strace:system/bin/strace \
-    device/motorola/edison/prebuilt/bin/finalize_release:system/bin/finalize_release \
-    device/motorola/edison/prebuilt/bin/uim-sysfs:system/bin/uim-sysfs \
-    device/motorola/edison/prebuilt/etc/gps.conf:system/etc/gps.conf \
-    device/motorola/edison/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
-    device/motorola/edison/prebuilt/etc/vold.fstab:system/etc/vold.fstab \
-    device/motorola/edison/prebuilt/usr/idc/evfwd.idc:system/usr/idc/evfwd.idc \
-    device/motorola/edison/prebuilt/usr/idc/Motorola_Mobility_Motorola_HD_Dock.idc:system/usr/idc/Motorola_Mobility_Motorola_HD_Dock.idc \
-    device/motorola/edison/prebuilt/usr/idc/qwerty.idc:system/usr/idc/qwerty.idc \
-    device/motorola/edison/prebuilt/usr/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
-    device/motorola/edison/prebuilt/usr/keychars/evfwd.kcm:system/usr/keychars/evfwd.kcm \
-    device/motorola/edison/prebuilt/usr/keychars/omap4-keypad.kcm:system/usr/keychars/omap4-keypad.kcm \
-    device/motorola/edison/prebuilt/usr/keychars/qwerty.kcm:system/usr/keychars/qwerty.kcm \
-    device/motorola/edison/prebuilt/usr/keychars/qwerty2.kcm:system/usr/keychars/qwerty2.kcm \
-    device/motorola/edison/prebuilt/usr/keylayout/atmxt-i2c.kl:system/usr/keylayout/atmxt-i2c.kl \
-    device/motorola/edison/prebuilt/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-    device/motorola/edison/prebuilt/usr/keylayout/cpcap-key.kl:system/usr/keylayout/cpcap-key.kl \
-    device/motorola/edison/prebuilt/usr/keylayout/cy8c201xx.kl:system/usr/keylayout/cy8c201xx.kl \
-    device/motorola/edison/prebuilt/usr/keylayout/evfwd.kl:system/usr/keylayout/evfwd.kl \
-    device/motorola/edison/prebuilt/usr/keylayout/omap4-keypad.kl:system/usr/keylayout/omap4-keypad.kl \
-    device/motorola/edison/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-    device/motorola/edison/prebuilt/usr/keylayout/usb_keyboard_102_en_us.kl:system/usr/keylayout/usb_keyboard_102_en_us.kl \
-    device/motorola/edison/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml \
-    device/motorola/edison/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf
+$(DEVICE_BLOB_FOLDER)/bin/battd:system/bin/battd \
+$(DEVICE_BLOB_FOLDER)/bin/mount_ext3.sh:system/bin/mount_ext3.sh \
+$(DEVICE_BLOB_FOLDER)/bin/strace:system/bin/strace \
+$(DEVICE_BLOB_FOLDER)/bin/finalize_release:system/bin/finalize_release \
+$(DEVICE_BLOB_FOLDER)/bin/uim-sysfs:system/bin/uim-sysfs \
+$(DEVICE_BLOB_FOLDER)/etc/gps.conf:system/etc/gps.conf \
+$(DEVICE_BLOB_FOLDER)/etc/media_profiles.xml:system/etc/media_profiles.xml \
+$(DEVICE_BLOB_FOLDER)/etc/vold.fstab:system/etc/vold.fstab \
+$(DEVICE_BLOB_FOLDER)/usr/idc/evfwd.idc:system/usr/idc/evfwd.idc \
+$(DEVICE_BLOB_FOLDER)/usr/idc/Motorola_Mobility_Motorola_HD_Dock.idc:system/usr/idc/Motorola_Mobility_Motorola_HD_Dock.idc \
+$(DEVICE_BLOB_FOLDER)/usr/idc/qwerty.idc:system/usr/idc/qwerty.idc \
+$(DEVICE_BLOB_FOLDER)/usr/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
+$(DEVICE_BLOB_FOLDER)/usr/keychars/evfwd.kcm:system/usr/keychars/evfwd.kcm \
+$(DEVICE_BLOB_FOLDER)/usr/keychars/omap4-keypad.kcm:system/usr/keychars/omap4-keypad.kcm \
+$(DEVICE_BLOB_FOLDER)/usr/keychars/qwerty.kcm:system/usr/keychars/qwerty.kcm \
+$(DEVICE_BLOB_FOLDER)/usr/keychars/qwerty2.kcm:system/usr/keychars/qwerty2.kcm \
+$(DEVICE_BLOB_FOLDER)/usr/keylayout/atmxt-i2c.kl:system/usr/keylayout/atmxt-i2c.kl \
+$(DEVICE_BLOB_FOLDER)/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+$(DEVICE_BLOB_FOLDER)/usr/keylayout/cpcap-key.kl:system/usr/keylayout/cpcap-key.kl \
+$(DEVICE_BLOB_FOLDER)/usr/keylayout/cy8c201xx.kl:system/usr/keylayout/cy8c201xx.kl \
+$(DEVICE_BLOB_FOLDER)/usr/keylayout/evfwd.kl:system/usr/keylayout/evfwd.kl \
+$(DEVICE_BLOB_FOLDER)/usr/keylayout/omap4-keypad.kl:system/usr/keylayout/omap4-keypad.kl \
+$(DEVICE_BLOB_FOLDER)/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+$(DEVICE_BLOB_FOLDER)/usr/keylayout/usb_keyboard_102_en_us.kl:system/usr/keylayout/usb_keyboard_102_en_us.kl \
+$(DEVICE_BLOB_FOLDER)/etc/media_codecs.xml:system/etc/media_codecs.xml \
+$(DEVICE_BLOB_FOLDER)/etc/audio_policy.conf:system/etc/audio_policy.conf
 
 PRODUCT_COPY_FILES += \
-    device/motorola/edison/prebuilt/lib/libwtnativetask.so:system/lib/libwtnativetask.so \
-    device/motorola/edison/prebuilt/lib/camera.omap4.so:system/lib/hw/camera.omap4.so
+$(DEVICE_BLOB_FOLDER)/lib/libwtnativetask.so:system/lib/libwtnativetask.so \
+$(DEVICE_BLOB_FOLDER)/lib/camera.omap4.so:system/lib/hw/camera.omap4.so
 
 PRODUCT_COPY_FILES += \
-    device/motorola/edison/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin:/system/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin \
-    device/motorola/edison/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin:/system/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin \
-    device/motorola/edison/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin:/system/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin \
-    device/motorola/edison/prebuilt/etc/firmware/ti-connectivity/wl128x-fw-4-mr.bin:/system/etc/firmware/ti-connectivity/wl128x-fw-4-mr.bin \
-    device/motorola/edison/prebuilt/etc/firmware/ti-connectivity/wl128x-fw-4-plt.bin:/system/etc/firmware/ti-connectivity/wl128x-fw-4-plt.bin \
-    device/motorola/edison/prebuilt/etc/firmware/ti-connectivity/wl128x-fw-4-sr.bin:/system/etc/firmware/ti-connectivity/wl128x-fw-4-sr.bin \
-    device/motorola/edison/prebuilt/etc/firmware/ti-connectivity/wl1271-nvs.bin:/system/etc/firmware/ti-connectivity/wl1271-nvs.bin \
-    device/motorola/edison/prebuilt/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin:/system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin
+$(DEVICE_BLOB_FOLDER)/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin:/system/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin \
+$(DEVICE_BLOB_FOLDER)/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin:/system/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin \
+$(DEVICE_BLOB_FOLDER)/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin:/system/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin \
+$(DEVICE_BLOB_FOLDER)/etc/firmware/ti-connectivity/wl128x-fw-4-mr.bin:/system/etc/firmware/ti-connectivity/wl128x-fw-4-mr.bin \
+$(DEVICE_BLOB_FOLDER)/etc/firmware/ti-connectivity/wl128x-fw-4-plt.bin:/system/etc/firmware/ti-connectivity/wl128x-fw-4-plt.bin \
+$(DEVICE_BLOB_FOLDER)/etc/firmware/ti-connectivity/wl128x-fw-4-sr.bin:/system/etc/firmware/ti-connectivity/wl128x-fw-4-sr.bin \
+$(DEVICE_BLOB_FOLDER)/etc/firmware/ti-connectivity/wl1271-nvs.bin:/system/etc/firmware/ti-connectivity/wl1271-nvs.bin \
+$(DEVICE_BLOB_FOLDER)/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin:/system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    device/motorola/edison/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
-    device/motorola/edison/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
-    device/motorola/edison/prebuilt/bin/50-backup.sh:system/addon.d/50-backup.sh
-
-# prebuilt hwc
-PRODUCT_COPY_FILES += \
-    #device/motorola/edison/hwc/hwcomposer.edison.so:system/vendor/lib/hw/hwcomposer.edison.so \
-    #device/motorola/edison/hwc/libedid.so:system/lib/libedid.so
+$(DEVICE_BLOB_FOLDER)/bin/backuptool.sh:system/bin/backuptool.sh \
+$(DEVICE_BLOB_FOLDER)/bin/backuptool.functions:system/bin/backuptool.functions \
+$(DEVICE_BLOB_FOLDER)/bin/50-backup.sh:system/addon.d/50-backup.sh
 
 # Temporarily use prebuilt DOMX
 # Prebuilts /system/lib
 PRODUCT_COPY_FILES += \
-    device/motorola/edison/prebuilt/lib/libdomx.so:/system/lib/libdomx.so \
-    device/motorola/edison/prebuilt/lib/libmm_osal.so:/system/lib/libmm_osal.so \
-    device/motorola/edison/prebuilt/lib/libOMX.TI.DUCATI1.MISC.SAMPLE.so:/system/lib/libOMX.TI.DUCATI1.MISC.SAMPLE.so \
-    device/motorola/edison/prebuilt/lib/libOMX.TI.DUCATI1.VIDEO.CAMERA.so:/system/lib/libOMX.TI.DUCATI1.VIDEO.CAMERA.so \
-    device/motorola/edison/prebuilt/lib/libOMX.TI.DUCATI1.VIDEO.DECODER.secure.so:/system/lib/libOMX.TI.DUCATI1.VIDEO.DECODER.secure.so \
-    device/motorola/edison/prebuilt/lib/libOMX.TI.DUCATI1.VIDEO.DECODER.so:/system/lib/libOMX.TI.DUCATI1.VIDEO.DECODER.so \
-    device/motorola/edison/prebuilt/lib/libOMX.TI.DUCATI1.VIDEO.H264E.so:/system/lib/libOMX.TI.DUCATI1.VIDEO.H264E.so \
-    device/motorola/edison/prebuilt/lib/libOMX.TI.DUCATI1.VIDEO.MPEG4E.so:/system/lib/libOMX.TI.DUCATI1.VIDEO.MPEG4E.so \
-    device/motorola/edison/prebuilt/lib/libOMX_Core.so:/system/lib/libOMX_Core.so
+$(DEVICE_BLOB_FOLDER)/lib/libdomx.so:/system/lib/libdomx.so \
+$(DEVICE_BLOB_FOLDER)/lib/libmm_osal.so:/system/lib/libmm_osal.so \
+$(DEVICE_BLOB_FOLDER)/lib/libOMX.TI.DUCATI1.MISC.SAMPLE.so:/system/lib/libOMX.TI.DUCATI1.MISC.SAMPLE.so \
+$(DEVICE_BLOB_FOLDER)/lib/libOMX.TI.DUCATI1.VIDEO.CAMERA.so:/system/lib/libOMX.TI.DUCATI1.VIDEO.CAMERA.so \
+$(DEVICE_BLOB_FOLDER)/lib/libOMX.TI.DUCATI1.VIDEO.DECODER.secure.so:/system/lib/libOMX.TI.DUCATI1.VIDEO.DECODER.secure.so \
+$(DEVICE_BLOB_FOLDER)/lib/libOMX.TI.DUCATI1.VIDEO.DECODER.so:/system/lib/libOMX.TI.DUCATI1.VIDEO.DECODER.so \
+$(DEVICE_BLOB_FOLDER)/lib/libOMX.TI.DUCATI1.VIDEO.H264E.so:/system/lib/libOMX.TI.DUCATI1.VIDEO.H264E.so \
+$(DEVICE_BLOB_FOLDER)/lib/libOMX.TI.DUCATI1.VIDEO.MPEG4E.so:/system/lib/libOMX.TI.DUCATI1.VIDEO.MPEG4E.so \
+$(DEVICE_BLOB_FOLDER)/lib/libOMX_Core.so:/system/lib/libOMX_Core.so
 
-# prebuilt ducati
+# more prebuilts (some temporary)
 PRODUCT_COPY_FILES += \
-    device/motorola/edison/prebuilt/etc/firmware/ducati-m3.bin:system/etc/firmware/ducati-m3.bin
+$(DEVICE_BLOB_FOLDER)/bin/libthermal-manager.cfg:system/bin/libthermal-manager.cfg \
+$(DEVICE_BLOB_FOLDER)/bin/thermaldaemon:system/bin/thermaldaemon \
+$(DEVICE_BLOB_FOLDER)/bin/vold:system/bin/vold \
+$(DEVICE_BLOB_FOLDER)/etc/firmware/ducati-m3.bin:system/etc/firmware/ducati-m3.bin \
+$(DEVICE_BLOB_FOLDER)/lib/libthermal_config.so:/system/lib/libthermal_config.so \
+$(DEVICE_BLOB_FOLDER)/lib/libthermal_manager.so:/system/lib/libthermal_manager.so
