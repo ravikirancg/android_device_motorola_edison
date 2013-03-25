7ca3f1503efcafd39e92961710103442ef63e53d
alteredlikeness <alteredlikeness@gmail.com>
2013-03-25 17:35:10 -0500

    further cleaning of device.mk
    STS-Dev-Team's omap4-common handles most of this already.

--- device.mk
+++ device.mk
@@ -5,46 +5,12 @@
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
-
 # Device overlay
     DEVICE_PACKAGE_OVERLAYS += device/motorola/edison/overlay
 
-# high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-    
-PRODUCT_PACKAGES := \
-    charger \
-    charger_res_images
-
 PRODUCT_PACKAGES += \
     camera.edison \
-    lights.edison \
-    libinvensense_mpl \
-    parse_hdmi_edid \
-    libedid 
-
-        #hwcomposer.omap4 \
-    #hwcomposer.default \
-PRODUCT_PACKAGES += \
-    libaudioutils \
-    libaudiohw_legacy 
-    
-     #libasound 
-    #libaudiopolicy_legacy
-    #audio.primary.edison \
-    #audio_policy.default \
-    #audio_policy.edison
-
-# BlueZ a2dp Audio HAL module
-PRODUCT_PACKAGES += \
-    audio.a2dp.default
-    
-# BlueZ test tools
-PRODUCT_PACKAGES += \
-    hciconfig \
-    hcitool
+    lights.edison
 
     # Tiler
 PRODUCT_PACKAGES += \
@@ -53,34 +19,8 @@ PRODUCT_PACKAGES += \
 # Modem
 PRODUCT_PACKAGES += \
     Stk \
-    libaudiomodemgeneric \
-    rild \
-    radiooptions \
     sh \
     nc 
-    #libreference-cdma-sms \
-
-# Wifi
-#    wlan_loader \
-#    wlan_cu \
-#    tiwlan.ini
-PRODUCT_PACKAGES += \
-    dhcpcd.conf \
-    hostapd.conf \
-    wifical.sh \
-    TQS_D_1.7.ini \
-    TQS_D_1.7_127x.ini \
-    crda \
-    regulatory.bin \
-    calibrator \
-    busybox
-
-# Wifi Direct
-PRODUCT_PACKAGES += \
-    ti_wfd_libs
-    
-    #lib_driver_cmd_wl12xx \
-    #wpa_supplicant.conf \
 
 # HotSpot
 #PRODUCT_PACKAGES += \
@@ -88,53 +28,6 @@ PRODUCT_PACKAGES += \
 #    tiap_cu \
 #    hostap \
 #    hostapd.conf 
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-    bt_sco_app 
-    #uim-sysfs 
-
-# Release utilities
-#PRODUCT_PACKAGES += \
-#    edison_releaseutils-check_kernel \
-#    edison_releaseutils-finalize_release \
-#    edison_releaseutils-mke2fs \
-#    edison_releaseutils-tune2fs
-
-PRODUCT_PACKAGES += \
-    Superuser \
-    su \
-    DockAudio \
-    Camera 
-   # camera_test \
-   # OMAPCamera 
-
-
-PRODUCT_PACKAGES += \
-    librs_jni \
-    com.android.future.usb.accessory \
-    FileManager \
-    MusicFX \
-    Apollo \
-    VideoEditor \
-    VoiceDialer \
-    Basic \
-    HoloSpiralWallpaper \
-    MagicSmokeWallpapers \
-    NoiseField \
-    Galaxy4 \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    VisualizationWallpapers \
-    PhaseBeam \
-    Gallery2 \
-    FileExplorer
-
-# we have enough storage space to hold precise GC data
-PRODUCT_TAGS += dalvik.gc.type-precise
-
-# still need to set english for audio init
-PRODUCT_LOCALES += en_US
 
 # copy all kernel modules under the "modules" directory to system/lib/modules
 PRODUCT_COPY_FILES += $(shell \
@@ -162,8 +55,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
 # stuff specific to ti OMAP4 hardware
-#$(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
-$(call inherit-product, device/motorola/edison/omap4.mk)
 $(call inherit-product, hardware/ti/wpan/ti-wpan-products.mk)
 
 $(call inherit-product, device/motorola/edison/device_edison-blobs.mk)
